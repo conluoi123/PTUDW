91f56bdcfe2fef76b1e39f85bc7fa78b65448001
23120353 <tranguyenquangthang2810@gmail.com>
2026-01-10 02:32:21 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from "express"
-import ENV from "./configs/env.configs";
+import ENV from "./configs/env.configs.js";
 const app = express(); 
 // đọc từ file .env 
 const PORT = ENV.PORT || 3000;
 
-// app.get('/', (req,res)=> {
-//     res.send('Web game');
-// })
+app.get('/', (req,res)=> {
+    res.send('Web game');
+})
 
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`)
+    console.log(`Server is listening on http://localhost:${PORT}`)
 })

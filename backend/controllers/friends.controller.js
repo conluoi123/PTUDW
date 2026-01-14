b03d48a551364b47ab2ee51ce262a9dccb6891cd
23120374 <ctinh1008@gmail.com>
2026-01-14 23:32:14 +0700

    [feat] friend controller - accept request

--- backend/controllers/friends.controller.js
+++ backend/controllers/friends.controller.js
@@ -11,3 +11,17 @@ export const sendRequest = async (req, res) => {
     res.status(500).json({ error: error.message })
   }
 }
+
+export const acceptRequest = async (req, res) => {
+  try {
+    const requesterId = req.params.id
+    const { currentUserId } = req.body
+    const result = await friendService.acceptRequest(requesterId, currentUserId)
+
+    res.status(200).json({ message: 'Friend request accepted', data: result })
+  } 
+  catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+

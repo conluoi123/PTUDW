42181294ba11ae420c71c46749c89e91af10f437
23120374 <ctinh1008@gmail.com>
2026-01-14 23:24:57 +0700

    [feat] friend model - create friendship

--- backend/models/friends.model.js
+++ backend/models/friends.model.js
@@ -6,3 +6,13 @@ export const findRelationship = async (userId1, userId2) => {
     .orWhere({ requester_id: userId2, addressee_id: userId1 })
     .first()
 }
+
+export const createFriendship = async (requesterId, addresseeId) => {
+  return await db('friendships')
+    .insert({
+      requester_id: requesterId,
+      addressee_id: addresseeId,
+      status: 'pending'
+    })
+    .returning('*')
+}

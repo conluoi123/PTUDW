a3dbdac93b0859f127e036180be394babf614514
Quoc113 <quoc113@gmail.com>
2026-01-15 02:00:57 +0700

    add component MessagePage with any ui in shadcn with hard code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { useState, useCallback, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, useNavigate, useLocation, Outlet } from 'react-router-dom';
 import { WelcomePage } from './components/pages/WelcomePage';
 import { ThemeProvider } from './contexts/ThemeContext';
-
+import { MessagesPage } from './components/pages/MessagePage';
 
 function App() {
     return (
-        <ThemeProvider>
-            <WelcomePage />
-        </ThemeProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<WelcomePage />} />
+                <Route path="/messages" element={<MessagesPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 

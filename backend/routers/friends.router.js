8cd85a910fca789e9201a94b0bd9719d851967ee
23120374 <ctinh1008@gmail.com>
2026-01-14 23:35:23 +0700

    [feat] friend router - send request

---
+++ backend/routers/friends.router.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import * as friendController from '../controllers/friends.controller.js'
+
+const router = express.Router()
+
+router.post('/request', friendController.sendRequest)

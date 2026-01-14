79314d4a12edcdb61ef46e16c85d6258e7ed6567
23120353 <tranguyenquangthang2810@gmail.com>
2026-01-14 16:46:37 +0700

    init google oauth2

--- backend/configs/env.configs.js
+++ backend/configs/env.configs.js
@@ -1,7 +1,11 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 const ENV = {
-    PORT: process.env.PORT,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-}
+  PORT: process.env.PORT,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  GOOGLE_REDIRECT_URL: process.env.GOOGLE_REDIRECT_URL,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_SECRET_CODE: process.env.GOOGLE_SECRET_CODE,
+  BACKEND_URL: process.env.BACKEND_URL,
+};
 export default ENV;

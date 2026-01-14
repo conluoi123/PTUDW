400b5e12337e1c2623ee8251faf6f830e4da7d81
23120353 <tranguyenquangthang2810@gmail.com>
2026-01-15 06:31:49 +0700

    add router update profile

--- backend/routers/user.routers.js
+++ backend/routers/user.routers.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { SignInWithGG, DirectGoogle, Register, Login, Logout, getProfile } from "../controllers/user.controllers.js";
+import { SignInWithGG, DirectGoogle, Register, Login, Logout, getProfile, updateProfile } from "../controllers/user.controllers.js";
 
 const userRouter = (app) => {
   const directRouter = Router();
@@ -9,6 +9,7 @@ const userRouter = (app) => {
   callbackRouter.get("/google/callback", SignInWithGG);
   app.use("/api/user/login", callbackRouter);
   const router = Router();
+  router.patch("/profile", updateProfile);
   router.get("/profile", getProfile);
   router.post("/logout", Logout);
   router.post("/login", Login);

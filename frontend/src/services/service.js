31de4567b256dae1a1bd7a03e26efe5e7241f93f
Quoc113 <quoc113@gmail.com>
2026-01-15 19:01:20 +0700

    fix: arg error

---
+++ frontend/src/services/service.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+
+// Xử lý nếu không xác thực được lại bằng refresh token thì ra login page
+// let onLogout = () => {};
+
+// export const setLogoutHandler = (logoutContext) => {
+//   onLogout = logoutContext;
+// };
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        "ngrok-skip-browser-warning": "true",
+    },
+});
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error) => {
+    failedQueue.forEach((p) => {
+        error ? p.reject(error) : p.resolve();
+    });
+    failedQueue = [];
+};
+
+api.interceptors.response.use(
+    (response) => response,
+
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response?.status !== 401) {
+            return Promise.reject(error);
+        }
+        if (originalRequest._retry) {
+            return Promise.reject(error);
+        }
+
+        originalRequest._retry = true;
+
+        if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+                failedQueue.push({
+                    resolve: () => resolve(api(originalRequest)),
+                    reject: (err) => reject(err),
+                });
+            });
+        }
+        isRefreshing = true;
+        try {
+            // await api.post("/auth/refreshToken");
+            await api.post("/api/user/refreshAccessToken");
+            processQueue(null);
+            return api(originalRequest);
+        } catch (refreshError) {
+            processQueue(refreshError);
+            //   if (onLogout) onLogout();
+            return Promise.reject(refreshError);
+        } finally {
+            isRefreshing = false;
+        }
+    }
+);
+
+export default api;

3f84741b736f3c37232d8e35d0936efce49606f0
23120353 <tranguyenquangthang2810@gmail.com>
2026-01-15 07:03:46 +0700

    complete get user by id for admin

--- backend/controllers/admin.controllers.js
+++ backend/controllers/admin.controllers.js
@@ -12,13 +12,21 @@ async function deleteUser(req, res) {
     }
 }
 
-async function getUserInfo() {
+async function getUserInfo(req, res) {
     try {
-        
-
+        const { userId } = req.params;
+        if (!userId) {
+            return res.status(400).json({ error: "Missing userId" });
+        }
+        const user = await User.findUserById(userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        return res.status(200).json({ message: "Get user successfully", user });
     } catch (error) {
-        
+        console.error(error);
+        return res.status(500).json({ error: "Internal server error" });
     }
 }
 
-export {deleteUser}
+export {deleteUser, getUserInfo}
--- backend/routers/admin.routers.js
+++ backend/routers/admin.routers.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { deleteUser } from "../controllers/admin.controllers.js";
+import { deleteUser, getUserInfo } from "../controllers/admin.controllers.js";
 
 const adminRouter = (app) => {
     const router = Router();
+    router.get("/:userId", getUserInfo);
     router.delete("/:userId", deleteUser);
     app.use("/api/admin", router);
 }

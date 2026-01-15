9de7722391da60eb7963c9732114f53c645f4acf
23120353 <tranguyenquangthang2810@gmail.com>
2026-01-15 07:57:03 +0700

    use middleware

--- backend/routers/admin.routers.js
+++ backend/routers/admin.routers.js
@@ -1,14 +1,20 @@
 import { Router } from "express";
-import { addUser, deleteUser, getAllUsers, getUserInfo, updateUser } from "../controllers/admin.controllers.js";
-
+import {
+  addUser,
+  deleteUser,
+  getAllUsers,
+  getUserInfo,
+  updateUser,
+} from "../controllers/admin.controllers.js";
+import { authenticateAccessToken } from "../middlewares/jwt.middlewares.js";
 const adminRouter = (app) => {
-    const router = Router();
-    router.get("/users", getAllUsers)
-    router.put("/:userId", updateUser);
-    router.post("/user", addUser);
-    router.get("/:userId", getUserInfo);
-    router.delete("/:userId", deleteUser);
-    app.use("/api/admin", router);
-}
+  const router = Router();
+  router.get("/users", authenticateAccessToken, getAllUsers);
+  router.put("/:userId", authenticateAccessToken, updateUser);
+  router.post("/user", authenticateAccessToken, addUser);
+  router.get("/:userId", authenticateAccessToken, getUserInfo);
+  router.delete("/:userId", authenticateAccessToken, deleteUser);
+  app.use("/api/admin", router);
+};
 
-export default adminRouter
+export default adminRouter;

7a4b963bf70a166f0a6525ac16cc571daac772c2
23120353 <tranguyenquangthang2810@gmail.com>
2026-01-15 15:56:41 +0700

    complete get my ranking api

--- backend/controllers/ranking.controllers.js
+++ backend/controllers/ranking.controllers.js
@@ -1,25 +1,37 @@
 import Ranking from "../models/ranking.models.js";
 
 async function getRankingGlobal(req, res) {
-    try {
-        const { gameId } = req.params;
-        if (!gameId) {
-            return res.status(400).json({ error: "Missing gameId" });
-        }
-        const ranking = await Ranking.rankingGlobal(gameId);
-        return res.status(200).json({message: "Get ranking succesfully", ranking})
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error" });
+  try {
+    const { gameId } = req.params;
+    if (!gameId) {
+      return res.status(400).json({ error: "Missing gameId" });
     }
+    const ranking = await Ranking.rankingGlobal(gameId);
+    return res
+      .status(200)
+      .json({ message: "Get ranking succesfully", ranking });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 }
 
 async function getRanking(req, res) {
-    
+  try {
+    const { gameId } = req.params;
+    if (!gameId) {
+      return res.status(400).json({ error: "Missing gameId" });
+    }
+    const ranking = await Ranking.rankingUser(gameId, req.userId);
+    return res
+      .status(200)
+      .json({ message: "Get ranking succesfully", ranking });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 }
 
-async function getRankingFriendList(req, res) {
-    
-}
+async function getRankingFriendList(req, res) {}
 
-export {getRanking, getRankingFriendList, getRankingGlobal}
+export { getRanking, getRankingFriendList, getRankingGlobal };

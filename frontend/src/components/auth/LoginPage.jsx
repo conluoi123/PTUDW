d974152502c56133edb64a6f0eec6c2f99ffb02b
23120353 <tranguyenquangthang2810@gmail.com>
2026-01-16 15:12:22 +0700

    init login and register page

---
+++ frontend/src/components/auth/LoginPage.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { Mail, Lock, Eye, EyeOff, Gamepad2, Zap, Shield, ArrowLeft } from 'lucide-react';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Link } from 'react-router-dom';
+
+
+export function LoginPage({ onLogin, onSwitchToRegister, onBack }) {
+    
+}
